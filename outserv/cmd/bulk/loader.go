dc6effe9ca2da47507e59b8cddf8262f3cdf24b1
Manish R Jain <manishrjain@gmail.com>
2022-07-14 15:17:16 -0700

    Removed loader.go

--- outserv/cmd/bulk/loader.go
+++
@@ -1,4 +0,0 @@
-// Portions Copyright 2017-2018 Dgraph Labs, Inc. are available under the Apache License v2.0.
-// Portions Copyright 2022 Outcaste LLC are available under the Sustainable License v1.0.
-
-package bulk
