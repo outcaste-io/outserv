48ab29359991db56d59d038f5fc2778c21035654
Manish R Jain <manishrjain@gmail.com>
2022-07-27 22:40:38 -0700

    code to bump UID from boot loader

--- outserv/cmd/alpha/run.go
+++ outserv/cmd/alpha/run.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"embed"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math"
 	"net"
@@ -17,6 +18,7 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"path"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -932,6 +934,17 @@ func run() {
 	// StartRaftNodes loads up DQL schema from disk. This is important step
 	// that's needed before GraphQL schema can be loaded in setupServer.
 	worker.StartRaftNodes(worker.State.WALstore, bindall)
+
+	// writeUIDFile in boot loader.
+	data, err := ioutil.ReadFile(path.Join(x.WorkerConfig.Dir.Posting, "max_uid"))
+	if err == nil {
+		maxUid, err := strconv.ParseUint(string(data), 0, 64)
+		if err == nil {
+			glog.Infof("Found Max UID in p directory: %#x\n", maxUid)
+			x.Check(zero.BumpMaxUid(context.Background(), maxUid))
+		}
+	}
+
 	atomic.AddUint32(&initDone, 1)
 
 	// initialization of the admin account can only be done after raft nodes are running
