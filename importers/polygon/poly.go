87540229ac8999ede62b1e2cef8f15509b5f399f
Manish R Jain <manishrjain@gmail.com>
2022-08-12 16:40:58 -0700

    write blocks and txns

--- importers/polygon/poly.go
+++ importers/polygon/poly.go
@@ -11,30 +11,35 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/pkg/errors"
 )
 
 var url = flag.String("url", "", "Bor URL address")
 var dst = flag.String("dst", "", "Outserv GraphQL endpoint, or a directory")
+var start = flag.Int64("start", 1, "Starting Block Number")
+var end = flag.Int64("end", 0, "If set, stop at this Block Number, including it")
+var gor = flag.Int("j", 4, "Num Goroutines to use")
 
-func uid(typ, id string) string {
+func uid(writer io.Writer, typ, id string) string {
 	if writer == nil {
 		return ""
 	}
 	return fmt.Sprintf("_:%s.%s", typ, id)
 }
 
-func fetchBlock(blockNumber int64) (*BlockParsed, error) {
+func fetchBlock(writer io.Writer, blockNumber int64) (*BlockParsed, error) {
+	fmt.Printf("Processing block: %d\n", blockNumber)
 	q := fmt.Sprintf(`{
 	"jsonrpc": "2.0",
 	"method": "eth_getBlockByNumber",
 	"params": ["%#x",true],
 	"id": 1}`, blockNumber)
 
-	fmt.Printf("url: %s\n q: %s\n", *url, q)
 	resp, err := http.Post(*url, "application/json", strings.NewReader(q))
 	if err != nil {
 		return nil, errors.Wrapf(err, "Unable to fetch block: %d", blockNumber)
@@ -44,21 +49,19 @@ func fetchBlock(blockNumber int64) (*BlockParsed, error) {
 	if err != nil {
 		return nil, errors.Wrapf(err, "Unable to read response")
 	}
-	fmt.Printf("Got resp:\n%s\n---\n", data)
 
 	type T struct {
 		Result BlockRPC `json:"result"`
 	}
 	var t T
 	Check(json.Unmarshal(data, &t))
-	fmt.Printf("Parsed block: %+v\n", t.Result)
 
 	var out BlockParsed
 	out.Block = t.Result.Block
-	out.Uid = uid("Block", out.Hash)
+	out.Uid = uid(writer, "Block", out.Hash)
 	if len(t.Result.Miner) > 0 {
 		out.Miner = Account{
-			Uid:     uid("Account", t.Result.Miner),
+			Uid:     uid(writer, "Account", t.Result.Miner),
 			Address: t.Result.Miner,
 		}
 	}
@@ -66,21 +69,16 @@ func fetchBlock(blockNumber int64) (*BlockParsed, error) {
 		var outTxn TransactionParsed
 		outTxn.Transaction = t.Transaction
 		outTxn.From = Account{
-			Uid:     uid("Account", t.From),
+			Uid:     uid(writer, "Account", t.From),
 			Address: t.From,
 		}
 		outTxn.To = Account{
-			Uid:     uid("Account", t.To),
+			Uid:     uid(writer, "Account", t.To),
 			Address: t.To,
 		}
-		outTxn.Uid = uid("Transaction", outTxn.Hash)
+		outTxn.Uid = uid(writer, "Transaction", outTxn.Hash)
 		out.Transactions = append(out.Transactions, outTxn)
 	}
-
-	// out.Transactions = out.Transactions[:1]
-	// data, err = json.MarshalIndent(out, " ", " ")
-	// Check(err)
-	// fmt.Printf("After parsing block:\n%s\n", data)
 	return &out, nil
 }
 
@@ -92,7 +90,7 @@ func processBlock(gid int, wg *sync.WaitGroup) {
 	if err != nil {
 		// This must be GraphQL endpoint.
 	} else if fi.IsDir() {
-		f, err := os.Create(filepath.Join(*dst, fmt.Sprintf("%02.json.gz", gid)))
+		f, err := os.Create(filepath.Join(*dst, fmt.Sprintf("%02d.json.gz", gid)))
 		Check(err)
 		gw := gzip.NewWriter(f)
 		writer = gw
@@ -104,8 +102,8 @@ func processBlock(gid int, wg *sync.WaitGroup) {
 		}()
 	}
 
-	for num := range c.blockCh {
-		block, err := fetchBlock(num)
+	for num := range blockCh {
+		block, err := fetchBlock(writer, num)
 		if err != nil {
 			fmt.Printf("Unable to parse block: %d\n", num)
 			Check(err)
@@ -115,7 +113,8 @@ func processBlock(gid int, wg *sync.WaitGroup) {
 			Check(err)
 			_, err = writer.Write(data)
 			Check(err)
-			return
+		} else {
+			// Send to Outserv directly
 		}
 	}
 }
@@ -126,9 +125,59 @@ func Check(err error) {
 	}
 }
 
+func latestBlock() (int64, error) {
+	q := `{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}`
+	resp, err := http.Post(*url, "application/json", strings.NewReader(q))
+	if err != nil {
+		return 0, errors.Wrapf(err, "while querying for latest block")
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, errors.Wrapf(err, "Unable to read response")
+	}
+	type T struct {
+		Result string `json:"result"`
+	}
+	var t T
+	if err := json.Unmarshal(data, &t); err != nil {
+		return 0, errors.Wrapf(err, "while unmarshal")
+	}
+	return strconv.ParseInt(t.Result, 0, 64)
+}
+
+var blockCh = make(chan int64, 16)
+
 func main() {
 	flag.Parse()
 
-	fmt.Println("vim-go")
-	fmt.Printf("fetch block with error: %v\n", fetchBlock(31706775))
+	bend, err := latestBlock()
+	Check(err)
+	fmt.Printf("Latest block: %d\n", bend)
+
+	wg := &sync.WaitGroup{}
+	for i := 0; i < *gor; i++ {
+		wg.Add(1)
+		go processBlock(i, wg)
+	}
+
+	curBlock := *start
+	for {
+		if *end != 0 {
+			if curBlock > *end {
+				break
+			}
+		}
+
+		if curBlock <= bend {
+			blockCh <- curBlock
+			curBlock++
+		} else {
+			time.Sleep(time.Second)
+			bend, err = latestBlock()
+			Check(err)
+		}
+	}
+	close(blockCh)
+	wg.Wait()
 }
