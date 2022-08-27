45ea213730234204c5e186748a346abbd1ee07da
Manish R Jain <manishrjain@gmail.com>
2022-08-27 00:12:55 -0700

    gzip output

--- importers/eth/script.sh
+++ importers/eth/script.sh
@@ -1,6 +1,7 @@
-rm $2.json
-for i in `seq $1 $2`; do cat query.txt |\
+rm $2.json.gz
+for i in `seq $1 $2`; do
+  cat query.txt |\
   sed "s/BNUM/$i/" |\
   curl $URL -XPOST -H "Content-Type: application/json" -d @- |\
-  jq -c .data.block >> $2.json
+  jq -c .data.block | pigz >> $2.json.gz
 done
