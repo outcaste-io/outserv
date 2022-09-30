02f4b7863ad5dd6694173a0475b98ef9347157a9
Pythonista7 <ashwin.mirskar@gmail.com>
2022-09-30 17:07:27 +0530

    init eth2-live , add marker file

--- importers/eth2/main.go
+++ importers/eth2/main.go
@@ -13,6 +13,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -22,13 +23,21 @@ import (
 	"github.com/outcaste-io/outserv/x"
 )
 
+const (
+	Fastx100EthApi  = "https://stream.0xfast.com/beta/eth-mainnet"
+	GethRPCEndpoint = "http://localhost:8545"
+)
+
 var url = flag.String("graphql", "", "Ethereum GraphQL endpoint")
 var numGo = flag.Int("gor", 8, "Number of goroutines to use")
 var blockRange = flag.Int64("block", 32, "Which block range to download in millions")
+var live = flag.Bool("live", false, "Polling geth client to catch up to the latest block and continuously keep updating")
 
 var curBlock, blockEnd int64
 var numBlocks uint64
 var numBytes uint64
+
+var dirName string
 
 const blockQuery = `{"query": "{block(number: %d) {baseFeePerGas,difficulty,extraData,gasLimit,gasUsed,hash,logsBloom,number,timestamp,miner{address},mixHash,nextBaseFeePerGas,nonce,number,ommers{hash},ommerHash,ommerCount,parent{hash},raw,rawHeader,receiptsRoot,stateRoot,timestamp,totalDifficulty,transactionsRoot,transactionCount,transactions{createdContract{address},cumulativeGasUsed,effectiveGasPrice,effectiveTip,from{address},gas,gasPrice,gasUsed,hash,index,inputData,maxFeePerGas,maxPriorityFeePerGas,to{address},nonce,r,raw,rawReceipt,s,status,type,v,value,logs{data,index,topics,account{address}}}}}"}`
 
@@ -105,24 +114,159 @@ func printQps() {
 	}
 }
 
-var dirName string
-
 func main() {
 	flag.Parse()
 	rand.Seed(time.Now().UnixNano())
 
-	var err error
-	dirName, err = ioutil.TempDir(".", fmt.Sprintf("%d-", *blockRange))
-	x.Check(err)
+	if !*live {
+		// Bulk update mode
+		var err error
+		dirName, err = ioutil.TempDir(".", fmt.Sprintf("%d-", *blockRange))
+		x.Check(err)
 
-	go printQps()
+		go printQps()
 
-	curBlock = (*blockRange * 1000000) - 1
-	blockEnd = curBlock + 1000000 // Included.
-	var wg sync.WaitGroup
-	for i := 0; i < *numGo; i++ {
-		wg.Add(1)
-		go processBlocks(i, &wg)
+		curBlock = (*blockRange * 1000000) - 1
+		blockEnd = curBlock + 1000000 // Included.
+		var wg sync.WaitGroup
+		for i := 0; i < *numGo; i++ {
+			wg.Add(1)
+			go processBlocks(i, &wg)
+		}
+		wg.Wait()
+	} else {
+		/*
+			Live update mode
+			   x1. Retrieve marker of last block processed AKA Current starting point.
+			      query [stream]/latest and get the latest block details and create a marker file + read it.
+
+			      if marker file is not present:
+					InitMarkerFile()
+
+				  else: marker file
+					{
+						ProcessedBlock: int64       | initial val : [stream]/latest
+						ProcessedBlockHash: string  | initial val : [stream]/latest
+						LatestBlockHeight: int64    | from geth api
+						LatestBlockHash: string     | from geth api
+						ts: time.Time>string        | initial val : time.Now()
+					}
+
+			   x2. Req geth client to get latest block number and update marker file.
+			   3. call processBlocks() : curBlock = ProcessedBlock + 1, blockEnd = LatestBlockHeight;
+				  result: new blocks are downloaded and stored as json.gz file.
+			   4. on complete update marker file with latest ProcessedBlock and ProcessedBlockHash.
+
+			TODO: update stream to use marker file when running parseFile() or add /bulk-write
+		*/
+
+		fmt.Print("Live update mode is not implemented yet.\n")
+		fmt.Printf("Using Fastx100 ETH : %s\n", Fastx100EthApi)
+
+		// fetch latest block from stream API
+		resp, err := http.Get(fmt.Sprintf("%s/latest", Fastx100EthApi))
+		x.Check(err)
+		out, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		x.Check(err)
+		if len(out) == 0 {
+			log.Fatal("latest block is empty")
+		}
+
+		var latestBlock BlockOut
+		x.Check(json.Unmarshal(out, &latestBlock))
+
+		// Open/Create marker file
+		mrkF, err := os.OpenFile("./marker.json", os.O_RDWR|os.O_CREATE, 0644)
+		x.Check(err)
+
+		defer func() {
+			mrkF.Sync()
+			mrkF.Close()
+		}()
+
+		byteVals, err := ioutil.ReadAll(mrkF)
+		x.Check(err)
+
+		latestBlockNumber, err := strconv.ParseInt(latestBlock.Number, 0, 64)
+		x.Check(err)
+
+		var marker Marker
+		if len(byteVals) == 0 {
+			// marker file is empty, init the marker file
+			fmt.Println("marker file not found. creating new marker file ...")
+
+			marker = Marker{
+				ProcessedBlock:      latestBlockNumber,
+				ProcessedBlockHash:  latestBlock.Hash,
+				Timestamp:           time.Now().UTC().String(),
+				LatestGethBlock:     -1,
+				LatestGethBlockHash: "",
+			}
+
+		} else {
+			// marker file is not empty, read the marker file
+			fmt.Println("marker file found. reading marker file ...")
+			x.Check(json.Unmarshal(byteVals, &marker))
+
+			// check the latest block on the [stream]/latest and update marker
+			if marker.ProcessedBlock < latestBlockNumber {
+				fmt.Printf("INFO: marker previously used on %s was at block:%d but new latest block on streams is %d , updating marker to start from new latestBlock. \n", marker.Timestamp, marker.ProcessedBlock, latestBlockNumber)
+
+				marker.ProcessedBlock = latestBlockNumber
+				marker.ProcessedBlockHash = latestBlock.Hash
+				marker.Timestamp = time.Now().UTC().String()
+
+			}
+
+		}
+		// fetch the latest block of the chain from geth client and update marker
+
+		// get geth status
+
+		// https://geth.ethereum.org/docs/rpc/graphql : Here’s how you’d get the latest block’s number
+		const gethBlockQuery = `{ "query": "query { block { number,hash } }" }`
+		const graphqlEndpoint = "http://localhost:8545/graphql"
+
+		fmt.Println("Fetching geth latest block ...")
+		resp, err = http.Post(graphqlEndpoint, "application/json", bytes.NewBufferString(gethBlockQuery))
+		if err != nil {
+			fmt.Println("graphql post req to geth node failed")
+			log.Fatal(err)
+		}
+		out, err = ioutil.ReadAll(resp.Body)
+		x.Check(err)
+		resp.Body.Close()
+		if len(out) == 0 {
+			fmt.Println("failed to read response body from geth graphql. Query : gethBlockQuery")
+			log.Fatal(err)
+		}
+
+		// update marker with latest block known to geth node
+
+		/* sample res :
+		{"data":{"block":{"number":15465208,
+		"hash":"0x2c486895b9e47ca119baa0da3b576234460886aef92a31b01f7abd3786395f7d"}}}
+		*/
+
+		var gethBlockRes GethBockResponse
+		x.Check(json.Unmarshal(out, &gethBlockRes))
+		marker.LatestGethBlock = gethBlockRes.Data.Block.Number
+		marker.LatestGethBlockHash = gethBlockRes.Data.Block.Hash
+		marker.Timestamp = time.Now().UTC().String()
+
+		if mrkData, err := json.Marshal(marker); err == nil {
+			mrkF.WriteAt(mrkData, 0)
+			x.Check(mrkF.Sync())
+		} else {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Marker :\n Latest Stream Block: %d \n Latest geth block: %d\n", marker.ProcessedBlock, marker.LatestGethBlock)
+
+		for i := marker.ProcessedBlock + 1; i <= marker.LatestGethBlock; i++ {
+			// Todo
+		}
 	}
-	wg.Wait()
+
 }
